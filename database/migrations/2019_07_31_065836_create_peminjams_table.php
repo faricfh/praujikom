d35b027c3cd27bd13f66432be6d425d4eeabfba9
faricfh <ifake377@gmail.com>
2019-07-31 14:53:45 +0700

    add migration

---
+++ database/migrations/2019_07_31_065836_create_peminjams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeminjamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('kode');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->integer('telp');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjams');
+    }
+}
